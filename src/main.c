d0b2e56d9c0da254929b27b642615ba10c254d66
Bluedillydilly <dylancuprewich@gmail.com>
2021-02-16 00:15:47 -0500

    file comments

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
+/*
+ * Author: Dylan Cuprewich (dxc2961), Steven Guan (sxg8944)
+ * Driver/tester for CSCI421 Group Project Storage Manager
+ * Simply to be used for testing functionality or debugging purposes.
+ * 
+ * NOTE: DO NOT PUT RELEVANT CODE HERE.
+ * PUT IMPORTANT CODE IN "storagemanager.c" OR SOME OTHER .c FILE!!!
+ * WE ARE NOT SUBMITTING "main.c"!
+ */
+
 #include <stdio.h>
 
 
 int main() {
-	//Phase 1
+	printf("Hello world!\n");	
 
 	return 0;
 }
--- src/storagemanager.c
+++ src/storagemanager.c
@@ -1,1 +1,12 @@
-#include "storagemanager.h"
+/*
+ * Author: Dylan Cuprewich (dxc2961), Steven Guan (sxg8944)
+ * Implements functions related to the storage manager
+ * for CSCI421 Group Project Storage Manager.
+ * Implements "include/storagemanager.h".
+ * 
+ * See "include/storagemanager.h" for more documentation.
+ * Functions w/o documentation here are described in header.
+ */
+
+#include "../include/storagemanager.h"
+
