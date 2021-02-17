19ecb1db0d188498e032ee87dd96570ce4f690b2
Bluedillydilly <dylancuprewich@gmail.com>
2021-02-17 18:32:44 -0500

    buffer manager stuffgit add include/buffer_manager.h src/buffer_manager.c ! TODO: finish LRU page removal

--- include/buffer_manager.h
+++ include/buffer_manager.h
@@ -7,11 +7,14 @@
 #ifndef BUFFERMANAGER_H
 #define BUFFERMANAGER_H 
 
+
 typedef struct buffer_manager {
     int current_page_count;
     int max_page_count;
     int min_page_id;
     int min_page_use_count;
+    union record_item**** pages; // SWICH TO PAGES
+    int* page_arr_with_count;
 }  buffer_manager;
 
 /*
@@ -20,6 +23,8 @@ typedef struct buffer_manager {
  * @return pointer to a new buffer_manager struct
  */
 buffer_manager* BufferManager_new(int max_page_count);
+
+int LRU_page_id(buffer_manager* buff_man);
 
 /*
  * Adds a page to the buffer to enable ease of use.
@@ -30,12 +35,28 @@ buffer_manager* BufferManager_new(int max_page_count);
 int add_page(buffer_manager* buff_man, union record_item*** page);
 
 /*
+ * Gets a page from the buffer manager
+ * @param buff_man - the buffer_manager that handles the pages.
+ * @param page_id - the page to be added to the buffer
+ * @param page - the page of interest
+ * @return 0 if the page was added successfully, otherwise -1;
+ */
+ int get_buffer_page(buffer_manager* buff_man, int page_id, union record_item*** page);
+
+/*
  * Removes a page from the buffer.
  * @param buff_man - the buffer manager that handles the pages.
  * @param page_index - the location of the page in the buffer
  * @return 0 if the page was successfully removed, otherwise -1;
  */
 int remove_page(buffer_manager* buff_man, int page_index);
+
+/*
+ * Frees up the members of a buffer_manager struct instance.
+ * @param buff_man - the buffer manager to be free'd up
+ * @return
+ */ 
+void destroy(buffer_manager* buff_man);
 
 
 #endif
---
+++ src/buffer_manager.c
@@ -0,0 +1,63 @@
+/*
+ * Author: Dylan Cuprewich (dxc2961)
+ * Implements functions related to the buffer manager
+ * for CSCI421 Group Project Storage Manager.
+ * Implements "include/buffer_manager.h".
+ * 
+ * See "include/buffer_manager.h" for more documentation.
+ */
+
+#include <stdlib.h>
+
+#include "../include/buffer_manager.h"
+#include "../include/storagemanager.h"
+
+buffer_manager* BufferManager_new(int max_page_count) {
+    buffer_manager* new_buff_man = malloc( sizeof(struct buffer_manager) );
+    new_buff_man->current_page_count = 0;
+    new_buff_man->max_page_count = 0;
+    new_buff_man->pages = malloc( max_page_count *
+         sizeof( union record_item )); //TODO SWITCH TO PAGES
+    new_buff_man->page_arr_with_count = malloc( max_page_count * sizeof(int) );
+    new_buff_man->min_page_id  = -1;
+    new_buff_man->min_page_use_count = 0;
+
+    return new_buff_man;
+}
+
+int add_page(buffer_manager* buff_man, union record_item*** page) {
+    if (buff_man->current_page_count < buff_man->max_page_count) {
+        buff_man->pages[buff_man->current_page_count] = page;
+        buff_man->current_page_count += 1;
+    } else { // buffer is full
+        int least_used_id = LRU_page_id(buff_man)
+        return -1;
+    }
+
+    return 0;
+}
+
+int remove_page(buffer_manager* buff_man, int page_index) {
+    for (int i = 0; i<buff_man->current_page_count-1; i++) {
+        buff_man->pages[i] = buff_man->pages[i+1];
+    }
+    buff_man->pages[ buff_man->current_page_count ] = 
+        malloc(sizeof( union record_item ));
+    buff_man->current_page_count -= 1;
+
+    return 0;
+}
+
+int get_buffer_page(buffer_manager* buff_man, int page_id, union record_item*** page) {
+    page = buff_man->pages[page_id];
+    buff_man->page_arr_with_count[page_id] += 1;
+
+    return 0;
+}
+
+void destroy(buffer_manager* buff_man) {
+    free(buff_man->pages);
+    free(buff_man->page_arr_with_count);
+
+    free(buff_man);
+}
