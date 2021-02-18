cd9984a87a86d7ba7054bcdf71a8bb93d7e0315e
sxg9999 <sxg9999@yahoo.com>
2021-02-17 19:09:46 -0500

    Added multiple functions to page.h

--- include/page.h
+++ include/page.h
@@ -12,6 +12,49 @@ typedef struct{
 }Page;
 
 
+/*
+Create and return a pointer to a Page struct
+@param records - a array of records thats contains record_items
+@returns a pointer to a page struct
+*/
 Page* Page_create(union record_item** records);
+
+/*
+Returns the records with the specified record_id in the table
+
+@param record_id - the id of the record in the table
+@returns an pointer to a array of record_items
+*/
+union record_item* get_record(int record_id);
+
+
+/*
+Inserts the provided record into the table witht he provided id.
+@param record - the record to be inserted to the page, an array of record_items
+@return 0 if record is successfully removed, -1 otherwise
+*/
+int insert_record(union record_item* record);
+
+
+/*
+Updates the provided record into the page with the provided page_id
+@param record_id
+@param record
+@return 0 if record is successfully removed, -1 otherwise
+*/
+int update_record(int record_id, union record_item* record);
+
+
+
+/*
+Removes the record specified by the key_values
+@param record_id
+@return 0 if record is successfully removed, -1 otherwise
+*/
+int remove_record(int record_id);
+
+
+
+
 
 #endif
