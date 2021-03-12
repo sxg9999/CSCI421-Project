cb6050909aff10826c4d13f190692f3b769e366c
Bluedillydilly <dylancuprewich@gmail.com>
2021-03-12 00:41:27 -0500

    database.c testing TEMP

--- src/database.c
+++ src/database.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 #include "../include/database.h"
+#include "../include/ddl_parser.h"
 
 int main(int argc, char* argv[] ) {
-    char test_string[] = "DROP TABLE NAME EROR";
+    char test_string[] = "DROP TABLE DRO;";
     printf("Input: %s\n", test_string);
     parse_ddl_statement(test_string);
 }
